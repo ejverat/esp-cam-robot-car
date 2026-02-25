1d7f02163eff176ccbc60ea8cc4e4a94b69e628e
Emmanuel Vera <emm.vera@gmail.com>
2026-02-25 01:10:36 -0600

    Add camera stream code based on example

--- lib/CarRobot/src/WebServer.cpp
+++ lib/CarRobot/src/WebServer.cpp
@@ -2,6 +2,7 @@
 #include "DriveController.hpp"
 #include "RobotGPIO.hpp"
 #include "esp32-hal-gpio.h"
+#include "esp_camera.h"
 #include "esp_http_server.h"
 #include <string_view>
 
@@ -54,17 +55,65 @@ esp_err_t action_handler(httpd_req_t *req) {
 }
 
 esp_err_t stream_handler(httpd_req_t *req) {
-  auto mCameraPtr = static_cast<Camera *>(req->user_ctx);
+
+  camera_fb_t *fb = NULL;
   esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t *_jpg_buf = NULL;
+  char *part_buf[64];
 
   res = httpd_resp_set_type(req, WebServer::_STREAM_CONTENT_TYPE);
-
-  if (res != ESP_OK)
+  if (res != ESP_OK) {
     return res;
+  }
 
-  httpd_resp_set_type(req, "text/html");
-  return httpd_resp_send(req, (const char *)WebServer::INDEX_HTML,
-                         strlen(WebServer::INDEX_HTML));
+  while (true) {
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      // Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    } else {
+      if (fb->width > 400) {
+        if (fb->format != PIXFORMAT_JPEG) {
+          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+          esp_camera_fb_return(fb);
+          fb = NULL;
+          if (!jpeg_converted) {
+            // Serial.println("JPEG compression failed");
+            res = ESP_FAIL;
+          }
+        } else {
+          _jpg_buf_len = fb->len;
+          _jpg_buf = fb->buf;
+        }
+      }
+    }
+    if (res == ESP_OK) {
+      size_t hlen =
+          snprintf((char *)part_buf, 64, WebServer::_STREAM_PART, _jpg_buf_len);
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+    }
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, WebServer::_STREAM_BOUNDARY,
+                                  strlen(WebServer::_STREAM_BOUNDARY));
+    }
+    if (fb) {
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    } else if (_jpg_buf) {
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if (res != ESP_OK) {
+      break;
+    }
+    // Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+  }
+  return res;
 }
 
 WebServer::WebServer(Camera camera, DriveController driver)
@@ -83,7 +132,7 @@ WebServer::WebServer(Camera camera, DriveController driver)
   httpd_uri_t stream_uri = {.uri = "/stream",
                             .method = HTTP_GET,
                             .handler = stream_handler,
-                            .user_ctx = &mCamera};
+                            .user_ctx = NULL};
   if (httpd_start(&camera_httpd, &config) == ESP_OK) {
     httpd_register_uri_handler(camera_httpd, &index_uri);
     httpd_register_uri_handler(camera_httpd, &cmd_uri);
